630f81fbb7f5854721a032407be18c428cec0077
neomaraiya <neomaraiya@outlook.com>
2021-05-14 19:37:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+// 1. Create two text box - first name and last name
+// 2. Focus the first text box on page load i.e first name
+// 3. Add a button "Add User".
+// 4. Add a list to display users (Child Component).
+// 5. Once user clicks on the "Add User", add the fullname to the list
 
 export default function App() {
   return (
