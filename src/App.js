5ef3d628ba7dbb5e863237cc3b84e5a4dca364cc
neomaraiya <neomaraiya@outlook.com>
2021-05-15 09:09:31 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 // 1. Create two text box - first name and last name
 // 2. Focus the first text box on page load i.e first name
 // 3. Add a button "Add User".
-// 4. Add a list to display users (Child Component).
+// 4. Add a list to display users
 // 5. Once user clicks on the "Add User", add the fullname to the list
 
 export default function App() {
